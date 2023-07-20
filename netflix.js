b08d85b9351912ca1da786f56cde4d423c382802
AryanSingh04 <140090637+AryanSingh04@users.noreply.github.com>
2023-07-20 20:16:20 +0530

    Add files via upload

---
+++ netflix.js
@@ -0,0 +1,29 @@
+let lq1 = document.getElementById("lq1");
+lq1.onclick = function(){
+lq1.classList.toggle("active");
+}
+let lq2 = document.getElementById("lq2");
+lq2.onclick = function(){
+lq2.classList.toggle("active");
+}
+let lq3 = document.getElementById("lq3");
+lq3.onclick = function(){
+lq3.classList.toggle("active");
+}
+let lq4 = document.getElementById("lq4");
+lq4.onclick = function(){
+lq4.classList.toggle("active");
+}
+let lq5 = document.getElementById("lq5");
+lq5.onclick = function(){
+lq5.classList.toggle("active");
+}
+let lq6 = document.getElementById("lq6");
+lq6.onclick = function(){
+lq6.classList.toggle("active");
+}
+
+let loade = document.getElementById("loade");
+  function load(){
+    loade.style.display = "none";
+  }
